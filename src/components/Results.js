9771d52a34b8412581772fad5cb981eca5f45dd7
abazurek <skala2294@gmail.com>
2020-11-09 18:32:07 +0100

    finished new modal window

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Modal from "react-modal";
+
 import Input from "./Input";
+
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 export default function Results({searchPhotos, setPhotos, photos, name, setName}) {
 
+    const [modal, setModal] = useState(false);
+    const [modalToShow, setModalToShow] = useState(false);
 
-    function showButtons(){
-        const tagsArray=[];
-        photos.results.forEach(item=>item.tags.forEach(tag=>{
-           if( !tagsArray.includes(tag.title)){
-               tagsArray.push(tag.title);
+    function showButtons() {
+        const tagsArray = [];
+        photos.results.forEach(item => item.tags.forEach(tag => {
+            if (!tagsArray.includes(tag.title)) {
+                tagsArray.push(tag.title);
 
-           }
+            }
         }))
         return (
             <div className='buttons-box container'>
-                {tagsArray.map(item=>
-                    <button onClick={(e)=> {
+                {tagsArray.map(item =>
+                    <button onClick={(e) => {
                         setName(item);
                         clickButton(e, item)
                     }} key={item}>{item}</button>)}
@@ -25,12 +32,26 @@ export default function Results({searchPhotos, setPhotos, photos, name, setName}
 
     }
 
-    function clickButton(e,item){
+    function clickButton(e, item) {
         e.preventDefault();
-        searchPhotos(item,setPhotos);
+        searchPhotos(item, setPhotos);
     }
 
 
+    function showModal(e, item) {
+        e.preventDefault();
+        setModal(true);
+        setModalToShow(
+            <div className='modal-box'>
+                <div className='author-box'>
+                    <p>Author: {item.user.name}</p>
+                    <FontAwesomeIcon onClick={() => setModal(false)} className='close-icon' icon={faTimes}/>
+                </div>
+                <img src={item.urls.regular} alt={item.description}/>
+                <p>Location: {item.user.location}</p>
+            </div>
+        )
+    }
 
     return (
         <section className='results-section'>
@@ -39,16 +60,41 @@ export default function Results({searchPhotos, setPhotos, photos, name, setName}
                 photos.results.length === 0 ?
                     <div className='container'>
                         <Input nameOfClass='in-results-section' searchPhotos={searchPhotos} setPhotos={setPhotos}/>
-                        <h1 style={{ marginTop: "100px"}}>no photos to show</h1>
+                        <h1 style={{marginTop: "100px"}}>no photos to show</h1>
                     </div>
                     :
                     <div>
                         <div className='container'>
-                            <Input nameOfClass='in-results-section' searchPhotos={searchPhotos} setPhotos={setPhotos} setName={setName}/>
+                            <Input nameOfClass='in-results-section' searchPhotos={searchPhotos} setPhotos={setPhotos}
+                                   setName={setName}/>
                             <h1>{name}</h1>
                         </div>
                         {showButtons()}
-                        {photos.results.map(item => <img src={item.urls.small} alt={item.title} key={item.id}/>)}
+                        <div className='photos-box'>
+                            {photos.results.map(item => <img onClick={(e) => showModal(e, item)} src={item.urls.small}
+                                                             alt={item.title} key={item.id}/>)}
+                        </div>
+
+                        <Modal isOpen={modal} onRequestClose={() => setModal(false)} style={{
+                            overlay: {
+                                backgroundColor: "none"
+                            },
+                            content: {
+                                position: 'absolute',
+                                top: '5%',
+                                left: '20%',
+                                width: "800px",
+                                height: "auto",
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                overflow: "auto",
+
+                            }
+                        }}>
+                            {modalToShow}
+                        </Modal>
                     </div>
                 :
                 ""
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import 'reset-css';
 import './scss/main.scss';
 
+import Modal from "react-modal";
+
 import App from './App';
 
+Modal.setAppElement('#root');
 
 ReactDOM.render(
   <React.StrictMode>
